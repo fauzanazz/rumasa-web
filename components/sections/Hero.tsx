fc8dc5680909f393d9214704038da7b72e98938b
M Fauzan Azhim <101320828+fauzanazz@users.noreply.github.com>
2025-10-04 20:48:09 +0700

    Update Hero.tsx

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -14,7 +14,7 @@ export function Hero() {
           {heroCopy.subheadline}
         </p>
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
-          <ScrollButton targetId="popular-designs">
+          <ScrollButton targetId="configurator">
             {heroCopy.secondary_cta}
           </ScrollButton>
         </div>
